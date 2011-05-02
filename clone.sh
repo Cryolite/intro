b4b270ae9218a24a264e23006dc92d4d4d5c0706
Cryolite <cryolite.indigo@gmail.com>
2011-05-02 14:46:30 +0900

    Introduced `mingw-w64-v1.0-snapshot-20110428.tar.bz2'. Moved the jamfile for `mingw-w64-headers'.

--- clone.sh
+++ clone.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 
-ln stage1/binutils/jamfile      stage2/binutils/jamfile
-ln stage1/gcc/jamfile           stage2/gcc/jamfile
-ln stage1/gmp/jamfile           stage2/gmp/jamfile
-ln stage1/mingw-w64-crt/jamfile stage2/mingw-w64-crt/jamfile
-ln stage1/mpc/jamfile           stage2/mpc/jamfile
-ln stage1/mpfr/jamfile          stage2/mpfr/jamfile
+ln stage1/binutils/jamfile          stage2/binutils/jamfile
+ln stage1/gcc/jamfile               stage2/gcc/jamfile
+ln stage1/gmp/jamfile               stage2/gmp/jamfile
+ln stage1/mingw-w64-crt/jamfile     stage2/mingw-w64-crt/jamfile
+ln stage1/mingw-w64-headers/jamfile stage2/mingw-w64-headers/jamfile
+ln stage1/mpc/jamfile               stage2/mpc/jamfile
+ln stage1/mpfr/jamfile              stage2/mpfr/jamfile
 
-ln stage1/binutils/jamfile      stage3/binutils/jamfile
-ln stage1/gcc/jamfile           stage3/gcc/jamfile
-ln stage1/gmp/jamfile           stage3/gmp/jamfile
-ln stage1/mingw-w64-crt/jamfile stage3/mingw-w64-crt/jamfile
-ln stage1/mpc/jamfile           stage3/mpc/jamfile
-ln stage1/mpfr/jamfile          stage3/mpfr/jamfile
+ln stage1/binutils/jamfile          stage3/binutils/jamfile
+ln stage1/gcc/jamfile               stage3/gcc/jamfile
+ln stage1/gmp/jamfile               stage3/gmp/jamfile
+ln stage1/mingw-w64-crt/jamfile     stage3/mingw-w64-crt/jamfile
+ln stage1/mingw-w64-headers/jamfile stage3/mingw-w64-headers/jamfile
+ln stage1/mpc/jamfile               stage3/mpc/jamfile
+ln stage1/mpfr/jamfile              stage3/mpfr/jamfile
