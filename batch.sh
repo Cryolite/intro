9721142d8a890ce65600839282cea7c64fb88ebc
Cryolite <cryolite.indigo@gmail.com>
2011-09-15 23:48:32 +0900

    Added and fixed wrapper scripts for `g++' and `clang++'.

--- batch.sh
+++ batch.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-prefix="${HOME}/local"
 dir="${0%/*}"
+if [ "$dir" != "." ]; then
+    dir="$(dirname "$0")"
+    if [ "$dir" = "." ]; then
+	echo "ERROR: this script should be called by the command containing '/'." 1>&2
+	exit 1
+    fi
+fi
+dir="$(cd "$dir"; pwd)"
+
+prefix="${HOME}/local"
 export PATH="${prefix}/bin:$PATH"
 export BOOST_ROOT="${prefix}/boost/latest"
-cd "$dir" && bjam -d+2 --triplet=x86_64-unknown-linux-gnu --prefix="$prefix" --with-mpi=openmpi --concurrency=3 --enable-awacs=\"${prefix}/sbin/twitter.rb\" current previous snapshot > $(date +%Y%m%d%H%M%S).log 2>&1
+cd "$dir" && bjam -d+2 --triplet=x86_64-unknown-linux-gnu --prefix="$prefix" --with-mpi=openmpi --enable-clang --concurrency=3 --enable-awacs=\"${prefix}/sbin/twitter.rb\" current previous snapshot > $(date +%Y%m%d%H%M%S).log 2>&1
