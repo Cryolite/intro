494d17eaafd360c66c54d13e0880d10647fb3ce5
Cryolite <cryolite.indigo@gmail.com>
2011-09-20 12:23:27 +0900

    No significant modification.

--- batch.sh
+++ batch.sh
@@ -11,6 +11,6 @@ fi
 dir="$(cd "$dir"; pwd)"
 
 prefix="${HOME}/local"
-export PATH="${prefix}/bin:$PATH"
+export PATH="${prefix}/bin${PATH:+:$PATH}"
 export BOOST_ROOT="${prefix}/boost/latest"
-cd "$dir" && bjam -d+2 --triplet=x86_64-unknown-linux-gnu --prefix="$prefix" --with-mpi=openmpi --enable-clang --concurrency=3 --enable-awacs=\"${prefix}/sbin/twitter.rb\" current previous snapshot > $(date +%Y%m%d%H%M%S).log 2>&1
+cd "$dir" && bjam -d+2 --triplet=x86_64-unknown-linux-gnu --prefix="$prefix" --enable-multilib --with-mpi=openmpi --enable-clang --concurrency=3 --enable-awacs=\"${prefix}/sbin/twitter.rb\" current previous snapshot > $(date +%Y%m%d%H%M%S).log 2>&1
