7947a5705d1dfa8820770558c382eb4853990a9d
Cryolite <cryolite.indigo@gmail.com>
2012-03-22 13:01:34 +0900

    Fixed #3.
    Modified the scripts to extract GCC Release Candidate versions.
    Fixed wrong specification of `ICU_PATH option' in `boost/jamfile'.

--- gcc-versions.sh
+++ gcc-versions.sh
@@ -8,60 +8,42 @@
 #url_prefix='http://mirrors-us.seosue.com/gcc'           # US
 url_prefix='http://ftp.tsukuba.wide.ad.jp/software/gcc' # Japan +++
 
-versions=`{ curl --silent "${url_prefix}/releases/" || exit 1; } \
-    | { grep -oE "gcc-[[:digit:]]+(\\.[[:digit:]]+){0,2}/" || exit 1; } \
-    | { sed -e 's/^gcc-\([[:digit:]]\{1,\}\(\.[[:digit:]]\{1,\}\)\{0,2\}\)\/$/\1/' || exit 1; } \
-    | { sort --version-sort --reverse || exit 1; } \
-    | { uniq || exit 1; }`
+builtin=`{ (LANG=C g++ -dumpversion) || exit 1; } \
+    | { tr --delete '\n' || exit 1; }`
 
-majors=`/bin/echo -n "$versions" \
-    | { grep -oE "^[[:digit:]]+\\.[[:digit:]]+" || exit 1; } \
+releases_src=`curl --silent "${url_prefix}/releases/" || exit 1`
+snapshots_src=`curl --silent "${url_prefix}/snapshots/" || exit 1`
+
+majors=`{ /bin/echo -n $snapshots_src || exit 1; } \
+    | { grep -oE "LATEST-[[:digit:]]+\\.[[:digit:]]+" || exit 1; } \
+    | { sed -e 's/^LATEST-\([[:digit:]]\{1,\}\.[[:digit:]]\{1,\}\)$/\1/' || exit 1; } \
     | { sort --version-sort --reverse || exit 1; } \
     | { uniq || exit 1; } \
     | { head --lines=4 || exit 1; }`
 
-builtin=`{ (LANG=C g++ --version) || exit 1; } \
-    | { head --lines=1 || exit 1; } \
-    | { grep -oE "[[:digit:]]+\\.[[:digit:]]+\\.[[:digit:]]+$" || exit 1; } \
-    | { tr --delete '\n' || exit 1; }`
+versions=
+for major in $majors; do
+    version=`{ /bin/echo -n $releases_src || exit 1; } \
+	| { grep -oE "gcc-${major}\\.[[:digit:]]+/" || exit 1; } \
+	| { sed -e 's/^gcc-\([[:digit:]]\{1,\}\(\.[[:digit:]]\{1,\}\)\{2\}\)\/$/\1/' || exit 1; } \
+	| { sort --version-sort --reverse || exit 1; } \
+	| { uniq || exit 1; } \
+	| { head --lines=1 || exit 1; }`
+    if [ -z "$version" ]; then
+	version=`{ /bin/echo -n $snapshots_src || exit 1; } \
+	    | { grep -oE "${major}(\\.0-RC)?-[[:digit:]]{8}" || exit 1; } \
+	    | { sort --version-sort --reverse || exit 1; } \
+	    | { uniq || exit 1; } \
+	    | { head --lines=1 || exit 1; }`
+    fi
+    if [ -z "$version" ]; then
+	exit 1;
+    fi
+    versions=${version}${versions:+ ${versions}}
+done
+versions=`{ /bin/echo $versions || exit 1; } \
+    | { tr ' ' '\n' || exit 1; } \
+    | { sort --version-sort --reverse || exit 1; }`
 
-current_major=`/bin/echo -n "$majors" \
-    | { head --lines=1 || exit 1; } \
-    | { tr --delete '\n' || exit 1; }`
-current=`/bin/echo -n "$versions" \
-    | { grep -oE "^$current_major(\\.[[:digit:]]+)?" || exit 1; } \
-    | { sort --version-sort --reverse || exit 1; } \
-    | { uniq || exit 1; } \
-    | { head --lines=1 || exit 1; } \
-    | { tr --delete '\n' || exit 1; }`
-
-previous_major=`/bin/echo -n "$majors" \
-    | { head --lines=2 || exit 1; } \
-    | { tail --lines=1 || exit 1; } \
-    | { tr --delete '\n' || exit 1; }`
-previous=`/bin/echo -n "$versions" \
-    | { grep -oE "^$previous_major(\\.[[:digit:]]+)?" || exit 1; } \
-    | { sort --version-sort --reverse || exit 1; } \
-    | { uniq || exit 1; } \
-    | { head --lines=1 || exit 1; } \
-    | { tr --delete '\n' || exit 1; }`
-
-oldest_major=`/bin/echo -n "$majors" \
-    | { head --lines=3 || exit 1; } \
-    | { tail --lines=1 || exit 1; } \
-    | { tr --delete '\n' || exit 1; }`
-oldest=`/bin/echo -n "$versions" \
-    | { grep -oE "^$oldest_major(\\.[[:digit:]]+)?" || exit 1; } \
-    | { sort --version-sort --reverse || exit 1; } \
-    | { uniq || exit 1; } \
-    | { head --lines=1 || exit 1; } \
-    | { tr --delete '\n' || exit 1; }`
-
-snapshot=`{ curl --silent "${url_prefix}/snapshots/" || exit 1; } \
-    | { grep -oE "[[:digit:]]+\\.[[:digit:]]+-[[:digit:]]{8}" || exit 1; } \
-    | { sort --version-sort --reverse || exit 1; } \
-    | { uniq || exit 1; } \
-    | { head --lines=1 || exit 1; } \
-    | { tr --delete '\n' || exit 1; }`
-
-/bin/echo -n "$builtin $current $previous $oldest $snapshot"
+/bin/echo -n $builtin $versions
+exit 0
