29d5cf671326c112354a4199905ffeab82396ee5
Cryolite <cryolite@ubuntu.(none)>
2012-07-25 08:58:35 +0900

    Improved download of binutils and mpc.
    Fixed bugs in MPC version extraction.
    Added multiple trials of MPC download.
    Added support of binutils snapshot download.

--- mpc-latest.sh
+++ mpc-latest.sh
@@ -1,10 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-uri_prefix='http://www.multiprecision.org/index.php?prog=mpc&page=download' #
-#uri_prefix='http://ftp.tsukuba.wide.ad.jp/software/mpc/'                    # Tsukuba Univ, Tsukuba, Ibaraki, Japan
-
-src=`curl --silent "$uri_prefix" || exit 1`
-src=`echo "$src" | { grep -Eo 'mpc-[[:digit:]]+(\.[[:digit:]]+){0,2}\.tar\.((gz)|(bz2))' || exit 1; }`
-src=`echo "$src" | { grep -Eo '^mpc-[[:digit:]]+(\.[[:digit:]]+){0,2}' || exit 1; }`
-src=`echo "$src" | { grep -Eo '[[:digit:]]+(\.[[:digit:]]+){0,2}$' || exit 1; }`
+src=`curl --silent 'http://www.multiprecision.org/index.php?prog=mpc&page=download'` || exit 1
+src=`echo "$src" | grep -Eo 'mpc-[[:digit:]]+(\.[[:digit:]]+){0,2}\.tar\.((gz)|(bz2))'` || exit 1
+src=`echo "$src" | grep -Eo '^mpc-[[:digit:]]+(\.[[:digit:]]+){0,2}'` || exit 1
+src=`echo "$src" | grep -Eo '[[:digit:]]+(\.[[:digit:]]+){0,2}$'` || exit 1
 echo "$src" | sort -u -t . -k 1,1n -k 2,2n -k 3,3n | tail --lines=1 | tr --delete '\n'
