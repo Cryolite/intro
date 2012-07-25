70396cd5dceba8607392fe4d10d9324d8b8a460e
Cryolite <cryolite.indigo@gmail.com>
2012-07-25 21:42:02 +0900

    Separated Clans trunk update scripts.

---
+++ clang/versions.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+versions=`curl --silent 'http://llvm.org/releases/download.html'` || exit 1
+versions=`echo -n "$versions" | grep -oE "Download LLVM [[:digit:]]+\\.[[:digit:]]+"` || exit 1
+versions=`echo -n "$versions" | grep -oE "[[:digit:]]+\\.[[:digit:]]+"` || exit 1
+versions=`echo -n "$versions" | sort -u -t . -k 1,1n -k 2,2n` || exit 1
+echo -n $versions
