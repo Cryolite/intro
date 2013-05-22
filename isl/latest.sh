a38d368848933a6fec5b84787f4fc82de2e31330
Cryolite <cryolite.indigo@gmail.com>
2013-05-22 09:47:11 +0900

    Added `HERE` and `HERE_RELATIVE`, and fixed bugs.
    * clang/jamfile: Fixed bugs in cloning and updating the repository.

--- isl/latest.sh
+++ isl/latest.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env sh
 
-intro_root=`(cd \`dirname "$0"\`; cd ..; pwd)` || exit 1
-[ -e "$intro_root/isl/latest.sh" ] || exit 1
+set -e
 
-if [ ! -d "$intro_root/isl-trunk" ]; then
-    ( cd "$intro_root" && git clone -q 'http://repo.or.cz/r/isl.git' isl-trunk )
-    if [ $? -ne 0 ]; then
-        ( cd "$intro_root" && rm -rf isl-trunk )
-        exit 1
-    fi
+intro_root=`(cd \`dirname "$0"\`; cd ..; pwd)`
+grep -Fq b3dd7826-a9ca-4a44-a951-788b5661ab27 "$intro_root/isl/latest.sh"
+
+#trap "( cd \"$intro_root\" && rm -r isl-trunk )" ERR HUP INT QUIT TERM
+
+# Checks whether ISL repository has been cloned into
+# `$intro_root/isl-trunk`. Note that `[ ! -d "$intro_root/isl-trunk" ]`
+# cannot be used because `bjam` would automatically create
+# `$intro_root/isl-trunk` directory.
+if [ ! -f "$intro_root/isl-trunk/ChangeLog" ]; then
+  ( cd "$intro_root" && git clone -q 'http://repo.or.cz/r/isl.git' isl-trunk )
 fi
 
-( cd "$intro_root/isl-trunk" && git pull -q )
-if [ $? -ne 0 ]; then
-    ( cd "$intro_root" && rm -rf isl-trunk )
-    exit 1
-fi
+( cd "$intro_root/isl-trunk" && git clean -ffdxq )
 
-src=`( cd "$intro_root/isl-trunk" && git tag )` || exit 1
-src=`echo "$src" | grep -Eo '^isl-[[:digit:]]+(\.[[:digit:]]+){0,2}$'` || exit 1
-src=`echo "$src" | grep -Eo '[[:digit:]]+(\.[[:digit:]]+){0,2}$'` || exit 1
+( cd "$intro_root/isl-trunk" && git pull --ff-only -q )
+
+src=`( cd "$intro_root/isl-trunk" && git tag )`
+src=`echo "$src" | grep -Eo '^isl-[[:digit:]]+(\.[[:digit:]]+){0,2}$'`
+src=`echo "$src" | grep -Eo '[[:digit:]]+(\.[[:digit:]]+){0,2}$'`
 echo "$src" | sort -u -t . -k 1,1n -k 2,2n -k 3,3n | tail --lines=1 | tr --delete '\n'
