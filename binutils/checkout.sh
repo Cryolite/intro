6fa6b342f5a7d8a7307bd5e7e1e2b3343c0f1c6d
Cryolite <cryolite.indigo@gmail.com>
2014-05-02 12:12:16 +0900

    Fixed #75.
    * util/git-checkout.sh: Newly added.
    * binutils/checkout.sh: Modified to delegate most of the work to
      `util/git-checkout.sh`.

--- binutils/checkout.sh
+++ binutils/checkout.sh
@@ -1,43 +1,14 @@
 #!/usr/bin/env bash
 
-intro_root=`(cd \`dirname "$0"\`; cd ..; pwd)`
-grep -Fq 5f91cd3a-6dac-42fe-bfbb-fe637ee880a4 "$intro_root/binutils/checkout.sh"
-
-PS4='+$0:$LINENO: '
+PS4='+binutils/checkout.sh:$LINENO: '
 set -ex
+
+intro_root="`(cd "\`dirname "$0"\`"; cd ..; pwd)`"
+grep -Fq 5f91cd3a-6dac-42fe-bfbb-fe637ee880a4 "$intro_root/binutils/checkout.sh"
 
 [ $# -eq 0 ]
 
-timestamp=cd9de9b7-e312-49f1-b2a5-24cd4f322ed5
-timestamp_path="$intro_root/binutils-trunk/$timestamp"
-
-if [ -e "$intro_root/binutils-trunk" ]; then
-  if [ -f "$timestamp_path" ]; then
-    ( cd "$intro_root/binutils-trunk" && [ `pwd` = `git rev-parse --show-toplevel` ] )
-    ( cd "$intro_root/binutils-trunk" && git clean -ffdx )
-  else
-    rm -r "$intro_root/binutils-trunk"
-  fi
-fi
-
-cleanup ()
-{
-  if [ -e "$intro_root/binutils-trunk" ]; then
-    [ -d "$intro_root/binutils-trunk" ] || {
-      echo 'warning: a logic error in rollback process, forced to proceed' >&2;
-    }
-    rm -r "$intro_root/binutils-trunk"
-  fi
-}
-trap cleanup ERR HUP INT QUIT TERM
-
-if [ '!' -e "$intro_root/binutils-trunk" ]; then
-  ( cd "$intro_root" && git clone 'git://sourceware.org/git/binutils-gdb.git' binutils-trunk )
-fi
-( cd "$intro_root/binutils-trunk" && [ `pwd` = `git rev-parse --show-toplevel` ] )
-
-( cd "$intro_root/binutils-trunk" && git pull --ff-only )
-
-last_commit_date=`cd "$intro_root/binutils-trunk" && LANG=C git log -1 --date=iso | grep -E '^Date:' | sed -e 's/^Date:[[:space:]]*\(.*\)$/\1/'`
-find "$intro_root/binutils-trunk" -execdir touch --date="$last_commit_date" '{}' \+
-touch --date="$last_commit_date" "$timestamp_path"
+( cd "$intro_root" && "$intro_root/util/git-checkout.sh" \
+    --url 'git://sourceware.org/git/binutils-gdb.git'    \
+    --srcdir binutils-trunk                              \
+    --timestamp cd9de9b7-e312-49f1-b2a5-24cd4f322ed5 )
