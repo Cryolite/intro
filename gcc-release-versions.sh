3cf1414dc0c8a0b6833ff0e5f9e61eac8ee1e802
Cryolite <cryolite.indigo@gmail.com>
2014-09-25 18:27:52 +0900

    Fixed #88.
    * .gitignore: Told to ignore GCC source tree directories with the name like
                  `gcc-5-20140921`.
    * gcc-release-versions.sh: Fixed extraction and manipulation of version
                               strings. See the description of #88 for more
                               detail.
    * compilers.jam: Likewise.

--- gcc-release-versions.sh
+++ gcc-release-versions.sh
@@ -1,34 +1,35 @@
 #!/usr/bin/env bash
 
-intro_root=`(cd \`dirname "$0"\`; pwd)`
-grep -Fq c20aed5f-b2d1-4ea8-af24-37acb51a17ec "$intro_root/gcc-release-versions.sh"
-
 set -e
+
+intro_root="$(cd "$(dirname "$0")"; pwd)"
+grep -Fq c20aed5f-b2d1-4ea8-af24-37acb51a17ec "$intro_root/gcc-release-versions.sh"
 
 urls=(http://{core.ring.gr.jp/pub/lang/egcs,ftp.dti.ad.jp/pub/lang/gcc,ftp.tsukuba.wide.ad.jp/software/gcc}/{releases,snapshots}/)
 timeout=30
 
-tempdir=`mktemp -d`
+tempdir="$(mktemp -d)"
 trap 'rm -rf "$tempdir"' ERR HUP INT TERM QUIT
 
 for url in "${urls[@]}"; do
-  curl -m $timeout -o `mktemp --tmpdir="$tempdir"` -s "$url" &
+    curl -m $timeout -o "$(mktemp --tmpdir="$tempdir")" -s "$url" &
 done
 wait
 
-versions=`cat "$tempdir"/*`
+versions="$(cat "$tempdir"/*)"
 rm -r "$tempdir"
-if echo "$versions" | grep -Eq '(gcc-[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)|([[:digit:]]+\.[[:digit:]]+(\.0-RC)?-[[:digit:]]{8})'; then
-  versions=`echo "$versions" | grep -Eo '(gcc-[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)|([[:digit:]]+\.[[:digit:]]+(\.0-RC)?-[[:digit:]]{8})'`
-  versions=`echo "$versions" | grep -Eo '[[:digit:]]+\.[[:digit:]]+((\.[[:digit:]]+)|((\.0-RC)?-[[:digit:]]{8}))'`
+
+if echo "$versions" | grep -Eq '(gcc-[[:digit:]]+(\.[[:digit:]]+){2})|([[:digit:]]+(\.[[:digit:]]+)?-[[:digit:]]{8})|([[:digit:]]+\.[[:digit:]]+\.0-RC-[[:digit:]]{8})'; then
+    versions="$(echo "$versions" | grep -Eo '(gcc-[[:digit:]]+(\.[[:digit:]]+){2})|([[:digit:]]+(\.[[:digit:]]+)?-[[:digit:]]{8})|([[:digit:]]+\.[[:digit:]]+\.0-RC-[[:digit:]]{8})')"
+    versions="$(echo "$versions" | grep -Eo '([[:digit:]]+(\.[[:digit:]]+){2})|([[:digit:]]+(\.[[:digit:]]+)?-[[:digit:]]{8})|([[:digit:]]+\.[[:digit:]]+\.0-RC-[[:digit:]]{8})')"
 fi
 
-local_versions=`cd "$intro_root" && ls -1 gcc-*/README 2>/dev/null || true`
-if echo "$local_versions" | grep -Eq '^gcc-[[:digit:]]+\.[[:digit:]]+((\.[[:digit:]]+)|((\.0-RC)?-[[:digit:]]{8}))/README$'; then
-  local_versions=`echo "$local_versions" | grep -Eo '[[:digit:]]+\.[[:digit:]]+((\.[[:digit:]]+)|((\.0-RC)?-[[:digit:]]{8}))'`
-  versions=`echo -e ${versions:+"$versions"'\n'}"$local_versions"`
+local_versions="$(cd "$intro_root" && ls -1 gcc-*/README 2>/dev/null || true)"
+if echo "$local_versions" | grep -Eq '^gcc-(([[:digit:]]+(\.[[:digit:]]+){2})|([[:digit:]]+(\.[[:digit:]]+)?-[[:digit:]]{8})|([[:digit:]]+\.[[:digit:]]+\.0-RC-[[:digit:]]{8}))/README$'; then
+    local_versions="$(echo "$local_versions" | grep -Eo '([[:digit:]]+(\.[[:digit:]]+){2})|([[:digit:]]+(\.[[:digit:]]+)?-[[:digit:]]{8})|([[:digit:]]+\.[[:digit:]]+\.0-RC-[[:digit:]]{8})')"
+    versions="$(echo -e ${versions:+"$versions"'\n'}"$local_versions")"
 fi
 
 [ -n "$versions" ]
-versions=`echo "$versions" | sort -u`
+versions="$(echo "$versions" | sort -u)"
 echo -n $versions
